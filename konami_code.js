c7822b96723d57512d4583b3dc3460eaf4864aea
Antonina Maj <antoninajanina@gmail.com>
2019-02-08 23:13:59 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,9 +17,18 @@ function init() {
   let index = 0;
   
   function KeyDown(e) {
-    
-    
-    
+    if (e.key === codes[index]) {
+      index++
+      if (index === codes.length) {
+        alert("Hurray!")
+        index = 0
+      }
+    } else {
+      index = 0
+    }
   }
   
+  document.body.addEventListener('keydown', KeyDown)
+  
 }
+
